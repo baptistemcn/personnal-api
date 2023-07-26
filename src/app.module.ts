17ed9b29d0705b3bf9f10fdc1a5d7787db7b64bc
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-26 18:01:58 +0200

    feat(api): add throller module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
-import { HealthModule } from "./health/health.module";
-import { RouterModule } from "@nestjs/core";
+import { APP_GUARD, RouterModule } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+
 import { AboutModule } from "./about/about.module";
+import { HealthModule } from "./health/health.module";
 
 @Module({
   imports: [
@@ -17,8 +19,17 @@ import { AboutModule } from "./about/about.module";
         module: AboutModule,
       },
     ]),
+    ThrottlerModule.forRoot({
+      ttl: 600,
+      limit: 5,
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
