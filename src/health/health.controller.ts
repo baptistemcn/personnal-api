14b48eda73efb9ce33371d13a5c5da1341c42c1c
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-26 14:41:39 +0200

    feat(health): add swagger documentation

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Header, HttpCode } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+
 import { HealthService } from "./health.service";
-import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags("Health")
 @Controller()
@@ -10,6 +11,19 @@ export class HealthController {
   @Get()
   @HttpCode(200)
   @Header("Content-Type", "application/json")
+  @ApiResponse({
+    status: 200,
+    description: "Health check",
+    content: {
+      "application/json": {
+        schema: {
+          properties: {
+            message: { type: "string" },
+          },
+        },
+      },
+    },
+  })
   check() {
     return this.healthService.check();
   }
