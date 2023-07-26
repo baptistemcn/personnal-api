5b2121240b86fd1980e078843fd4503e898620cf
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-26 12:47:48 +0200

    feat(about): add swagger documentation

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Header, HttpCode } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+
 import { AboutService } from "./about.service";
-import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags("About")
 @Controller()
@@ -10,6 +11,38 @@ export class AboutController {
   @Get()
   @HttpCode(200)
   @Header("Content-Type", "application/json")
+  @ApiResponse({
+    status: 200,
+    description: "Get all experiences",
+    content: {
+      "application/json": {
+        schema: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              name: { type: "string" },
+              presentation: { type: "string" },
+              duration: { type: "string" },
+              technologies: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    name: { type: "string" },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: "Server Error",
+  })
   findAll() {
     return this.aboutService.findAll();
   }
