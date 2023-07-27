b9f5ac9d6ca1e6956f36819a3ae24b8fbc2fc3fa
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-27 19:02:14 +0200

    chore(swagger): disable in production

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle("Baptiste Marcon API")
-    .setDescription("Baptiste Marcon API swagger")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
+  if (process.env.NODE_ENV !== "production") {
+    const config = new DocumentBuilder()
+      .setTitle("Baptiste Marcon API")
+      .setDescription("Baptiste Marcon API swagger")
+      .setVersion("1.0")
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("swagger", app, document);
+  }
 
   await app.listen(3000);
 }
