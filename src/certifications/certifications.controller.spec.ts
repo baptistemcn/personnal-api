9d5a5209046ae05afc9c2d1e4224d0e5e84b85cf
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-27 17:33:35 +0200

    test(certifications): add unit tests

--- src/certifications/certifications.controller.spec.ts
+++ src/certifications/certifications.controller.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CertificationsController } from "./certifications.controller";
 import { CertificationsService } from "./certifications.service";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const mockFilePath = join(process.cwd(), "assets/certifications.json");
+const mockFileContent = readFileSync(mockFilePath, "utf-8");
+const mockJsonData = JSON.parse(mockFileContent);
 
 describe("CertificationsController", () => {
   let controller: CertificationsController;
+  let provider: CertificationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +19,16 @@ describe("CertificationsController", () => {
     }).compile();
 
     controller = module.get<CertificationsController>(CertificationsController);
+    provider = module.get<CertificationsService>(CertificationsService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("should return findAll from service", () => {
+    const result = provider.findAll();
+
+    expect(result).toEqual(mockJsonData);
   });
 });
--- src/certifications/certifications.service.spec.ts
+++ src/certifications/certifications.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CertificationsService } from "./certifications.service";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const mockFilePath = join(process.cwd(), "assets/certifications.json");
+const mockFileContent = readFileSync(mockFilePath, "utf-8");
+const mockJsonData = JSON.parse(mockFileContent);
 
 describe("CertificationsService", () => {
   let service: CertificationsService;
@@ -14,5 +20,11 @@ describe("CertificationsService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should return a JSON object of certifications", () => {
+    const result = service.findAll();
+
+    expect(result).toEqual(mockJsonData);
   });
 });
