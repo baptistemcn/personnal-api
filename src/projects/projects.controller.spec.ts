1acf80a0da703b88939a703cd6478d18ac5cfb32
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-27 18:40:31 +0200

    test(projects): add unit tests

--- src/projects/projects.controller.spec.ts
+++ src/projects/projects.controller.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ProjectsController } from "./projects.controller";
 import { ProjectsService } from "./projects.service";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const mockFilePath = join(process.cwd(), "assets/projects.json");
+const mockFileContent = readFileSync(mockFilePath, "utf-8");
+const mockJsonData = JSON.parse(mockFileContent);
 
 describe("ProjectsController", () => {
   let controller: ProjectsController;
+  let provider: ProjectsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +19,16 @@ describe("ProjectsController", () => {
     }).compile();
 
     controller = module.get<ProjectsController>(ProjectsController);
+    provider = module.get<ProjectsService>(ProjectsService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("should return findAll from service", () => {
+    const result = provider.findAll();
+
+    expect(result).toEqual(mockJsonData);
   });
 });
--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ProjectsService } from "./projects.service";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const mockFilePath = join(process.cwd(), "assets/projects.json");
+const mockFileContent = readFileSync(mockFilePath, "utf-8");
+const mockJsonData = JSON.parse(mockFileContent);
 
 describe("ProjectsService", () => {
   let service: ProjectsService;
@@ -14,5 +20,11 @@ describe("ProjectsService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should return a JSON objects of projects", () => {
+    const result = service.findAll();
+
+    expect(result).toEqual(mockJsonData);
   });
 });
